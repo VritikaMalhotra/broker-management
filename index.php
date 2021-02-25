af83a6524c74aec6e14629701185e67aabf955cc
marz-20 <170320116057.it.mayank@gmail.com>
2021-02-25 17:13:51 +0530

    feature theme commit Mayank Thacker 25-02-2021

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include 'head.php';?>
+
+</head>
+<body>
+<?php include 'header.php';?>
+<?php include 'sidebar.php';?>
+<?php include 'footer.php';?>
+
+</body>
+</html>
